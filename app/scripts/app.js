5140ab89e79ab5ecb6152bb2d62029cacee87248
sriharsha gundappa <harshag7@gmail.com>
2015-07-28 22:47:35 -0400

    Initial Deployment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,12 +5,31 @@ $(document).ready(function()
 	applyHeader(); 
 });
 
-/* HEADER FUNCTIONS */
 
 function applyHeader()
 {
 	$('.jumbotron').css({ height: ($(window).height()) +'px' });
 }
+
+$(window).scroll(function() {
+    var jumbotron = $('.jumbotron');
+    if($('.navbar').offset().top > jumbotron.height()) {
+        $('.navbar-fixed-top').addClass('top-nav-collapse');
+    } else {
+        $('.navbar-fixed-top').removeClass('top-nav-collapse');
+    }
+    
+});
+
+$(function(){
+	$('.page-scroll a').bind('click', function(){
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+});
 
 /**
  * @ngdoc overview
